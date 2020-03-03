2d64d33f0b0b01df6b892e88ef19c118dc0a4565
Roland Tritsch <roland@tritsch.org>
2020-03-03 13:23:23 +0000

    Package is updating the init file with this.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,18 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/roland/00-roland.org"))
+
+;; do not edit below this line
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-mode zenburn-theme yaml-mode which-key use-package ubuntu-theme scala-mode sbt-mode protobuf-mode org-bullets org-beautify-theme org monochrome-theme minimal-theme markdown-toc magit lsp-ui leuven-theme kosmos-theme json-mode intero flycheck-plantuml disable-mouse company-lsp basic-theme autumn-light-theme arjen-grey-theme apropospriate-theme anti-zenburn-theme alchemist afternoon-theme ace-window abyss-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
