8cb4d638c2451b6120c8cc72995418b97443c209
Roland Tritsch <roland@tritsch.email>
2026-01-13 07:56:28 +0000

    Setup rust (but disable it)

--- init.el
+++ init.el
@@ -7,13 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-    '(add-node-modules-path aidermacs claude-code-ide company counsel default-text-scale disable-mouse
-       editorconfig elisp-format forge gptel htmlize json-mode lorem-ipsum lsp-metals lsp-ui
-       markdown-toc org-download org-mind-map org-roam ox-reveal sbt-mode tide transpose-frame
-       typescript-mode vterm which-key windresize yaml-mode))
- '(package-vc-selected-packages
-    '((claude-code-ide :url "https://github.com/manzaltu/claude-code-ide.el"))))
+ '(package-selected-packages nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
