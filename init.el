4a2248f9ed6b49248806590b688c9937c394077a
Roland Tritsch <roland@tritsch.email>
2026-01-13 08:02:52 +0000

    Update/Upgrade packages.

--- init.el
+++ init.el
@@ -7,7 +7,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(add-node-modules-path claude-code-ide counsel default-text-scale disable-mouse
+                           elisp-format elpy forge htmlize json-mode lorem-ipsum
+                           lsp-metals lsp-ui markdown-toc org-download
+                           org-mind-map org-roam ox-reveal python-isort
+                           rust-mode sbt-mode tide transpose-frame
+                           typescript-mode vterm windresize yaml-mode)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
