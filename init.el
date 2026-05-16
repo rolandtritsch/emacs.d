f819794830598dcd650798ff70e78a48ba3720ae
Roland Tritsch <roland@tritsch.email>
2026-05-16 21:14:50 +0100

    WIP

--- init.el
+++ init.el
@@ -7,8 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
- '(package-vc-selected-packages '((codex :url "https://github.com/benthamite/codex"))))
+ '(package-vc-selected-packages
+   '((claude-code-ide :url "https://github.com/manzaltu/claude-code-ide.el"))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
