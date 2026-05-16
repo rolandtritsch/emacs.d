5ff31098a032c27c9c2c92d8c8960fa756500c73
Roland Tritsch <roland@ailtir.com>
2026-05-16 21:22:28 +0100

    Fix org startup package loading

--- init.el
+++ init.el
@@ -1,3 +1,24 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el"
+                         user-emacs-directory))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(eval-and-compile
+  (setq straight-use-package-by-default t
+        use-package-always-ensure nil
+        use-package-expand-minimally t))
+
+(straight-use-package 'org)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/roland/00-roland.org"))
 
 ;; do not edit below this line
